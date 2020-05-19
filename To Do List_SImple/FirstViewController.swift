c98fb883b4843a7948549f96584d78fa8efcc4c3
trungld3 <k21tpm4@gmail.com>
2020-05-19 11:34:27 +0700

    Initial Commit

---
+++ To Do List_SImple/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  To Do List_SImple
+//
+//  Created by TrungLD on 5/19/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ To Do List_SImple/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  To Do List_SImple
+//
+//  Created by TrungLD on 5/19/20.
+//  Copyright © 2020 TrungLD. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
